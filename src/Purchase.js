ec9bea35735e35bd94131012715399a30c9a78b2
Justin Licari <justinlicari@toram2anjg5h.cbc-rc.ca>
2020-02-05 15:26:58 -0500

    t

--- src/Purchase.js
+++ src/Purchase.js
@@ -8,12 +8,19 @@ function Purchase() {
         if (products.length > 0) {
             let total = 0
             products.forEach(productCode => {
+                console.log(total)
+                console.log(productCode)
                 total += priceDirectory[productCode]
             });
-            return total
+            return new Money(total)
         } else {
             return new Money(0)
         }
+    }
+
+    this.add = (productID) => {
+        products.push(productID)
+        console.log(products)
     }
 }
 
--- test/Purchase.test.js
+++ test/Purchase.test.js
@@ -2,6 +2,7 @@ var sinon = require('sinon');
 var Money = require('../lib/DontTouch/util/Money');
 var PAGAClient = require('../lib/DontTouch/paga/PAGAClientImpl');
 var Purchase = require('../src/Purchase');
+var priceDirectory = require('../src/constants')
 
 describe('CheckOut test suite', function () {
   var mockPAGAClient;
@@ -15,5 +16,15 @@ describe('CheckOut test suite', function () {
   test('total is zero initially', function () {
     const purchase = new Purchase();
     expect(purchase.total().equals(new Money(0))).toEqual(true)
+  });
+
+  test('adding first item results in a total equal to product price', function () {
+    const purchase = new Purchase();
+    const productID = "000000001234"
+
+    purchase.add(productID)
+    console.log(purchase.total())
+    console.log(priceDirectory["000000001234"])
+    expect(purchase.total().equals(priceDirectory["000000001234"])).toEqual(true)
   });
 });
